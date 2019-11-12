56d6d4206da2022929ed319638456dddcb9a5e90
AdouniH <houssemadouni11@gmail.com>
2019-11-12 18:45:31 +0100

    save changes

--- btg_app/views.py
+++ btg_app/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 def index(request):
     context = {}
     return render(request, 'index.html', context)
+
+def contact(request):
+    context = {}
+    return render(request, 'contact.html', context)
